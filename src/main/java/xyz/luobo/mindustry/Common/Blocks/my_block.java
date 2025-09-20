38cfa266d5671a8e13031bd1ba5df558773c62bd
luobochuanqi <luobochuanqi@qq.com>
2025-09-21 00:34:45 +0800

    feat: add GeckoLib repository and implement my_block class for custom block model

---
+++ src/main/java/xyz/luobo/mindustry/Common/Blocks/my_block.java
@@ -0,0 +1,22 @@
+package xyz.luobo.mindustry.Common.Blocks;
+
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.animatable.GeoBlockEntity;
+import software.bernie.geckolib.model.GeoModel;
+
+public class my_block extends GeoModel<GeoBlockEntity> {
+    @Override
+    public ResourceLocation getModelResource(GeoBlockEntity animatable) {
+        return null;
+    }
+
+    @Override
+    public ResourceLocation getTextureResource(GeoBlockEntity animatable) {
+        return null;
+    }
+
+    @Override
+    public ResourceLocation getAnimationResource(GeoBlockEntity animatable) {
+        return null;
+    }
+}
