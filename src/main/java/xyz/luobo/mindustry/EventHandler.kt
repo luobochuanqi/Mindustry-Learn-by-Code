82ac2e2abc3ea6873242e392da6fe9b6a8912c66
luobochuanqi <luobochuanqi@qq.com>
2025-10-21 16:31:36 +0800

    refactor: 👋 Java 再见

--- src/main/java/xyz/luobo/mindustry/EventHandler.kt
+++ src/main/java/xyz/luobo/mindustry/EventHandler.kt
@@ -1,44 +1,45 @@
-package xyz.luobo.mindustry;
+package xyz.luobo.mindustry
 
-import com.mojang.logging.LogUtils;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.IModBusEvent;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
-import net.neoforged.neoforge.client.event.EntityRenderersEvent;
-import org.slf4j.Logger;
-import xyz.luobo.mindustry.Client.Renderers.PowerNodeBlockEntityRenderer;
-import xyz.luobo.mindustry.Common.ModBlockEntities;
+import com.mojang.logging.LogUtils
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider
+import net.neoforged.api.distmarker.Dist
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.fml.event.IModBusEvent
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
+import net.neoforged.fml.event.lifecycle.FMLDedicatedServerSetupEvent
+import net.neoforged.neoforge.client.event.EntityRenderersEvent.RegisterRenderers
+import org.slf4j.Logger
+import xyz.luobo.mindustry.Client.Renderers.PowerNodeBlockEntityRenderer
+import xyz.luobo.mindustry.Common.BlockEntities.PowerNodeBlockEntity
+import xyz.luobo.mindustry.Common.ModBlockEntities.POWER_NODE_BLOCK_ENTITY
 
-public class EventHandler {
-    private static final Logger LOGGER = LogUtils.getLogger();
+object EventHandler {
+    private val LOGGER: Logger = LogUtils.getLogger()
 
     // 此为客户端事件总线订阅器
-    @EventBusSubscriber(modid = Mindustry.MOD_ID, value = Dist.CLIENT)
-    public static class ClientModEvents implements IModBusEvent {
+    @EventBusSubscriber(modid = Mindustry.MOD_ID, value = [Dist.CLIENT])
+    object ClientModEvents : IModBusEvent {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            LOGGER.info("HELLO FROM Client SETUP");
+        fun onClientSetup(event: FMLClientSetupEvent?) {
+            LOGGER.info("HELLO FROM Client SETUP")
         }
-        @SubscribeEvent // on the mod event bus only on the physical client
-        public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
-            event.registerBlockEntityRenderer(
-                    // The block entity type to register the renderer for.
-                    ModBlockEntities.INSTANCE.getPOWER_NODE_BLOCK_ENTITY().get(),
-                    // A function of BlockEntityRendererProvider.Context to BlockEntityRenderer.
-                    PowerNodeBlockEntityRenderer::new
-            );
+
+        @SubscribeEvent
+        fun registerEntityRenderers(event: RegisterRenderers) {
+            event.registerBlockEntityRenderer<PowerNodeBlockEntity>( // The block entity type to register the renderer for.
+                POWER_NODE_BLOCK_ENTITY.get(),  // A function of BlockEntityRendererProvider.Context to BlockEntityRenderer.
+                BlockEntityRendererProvider(::PowerNodeBlockEntityRenderer)
+            )
         }
     }
 
     // 此为服务端事件总线订阅器
-    @EventBusSubscriber(modid = Mindustry.MOD_ID, value = Dist.DEDICATED_SERVER)
-    public static class ServerModEvents implements IModBusEvent {
+    @EventBusSubscriber(modid = Mindustry.MOD_ID, value = [Dist.DEDICATED_SERVER])
+    object ServerModEvents : IModBusEvent {
         @SubscribeEvent
-        public static void onDedicatedServerSetup(FMLDedicatedServerSetupEvent event) {
-            LOGGER.info("HELLO FROM Dedicated Server SETUP");
+        fun onDedicatedServerSetup(event: FMLDedicatedServerSetupEvent?) {
+            LOGGER.info("HELLO FROM Dedicated Server SETUP")
         }
     }
 }
