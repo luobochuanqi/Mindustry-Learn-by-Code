bc27e53231a718e6a414c80255b97968fce9615a
luobochuanqi <luobochuanqi@qq.com>
2025-10-23 23:05:35 +0800

    feat: add data generation for localization and item models

--- src/main/java/xyz/luobo/mindustry/DataGen.kt
+++ src/main/java/xyz/luobo/mindustry/DataGen.kt
@@ -2,6 +2,7 @@ package xyz.luobo.mindustry
 
 import net.minecraft.data.DataGenerator
 import net.minecraft.data.PackOutput
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import net.neoforged.neoforge.common.data.LanguageProvider
@@ -13,9 +14,11 @@ object DataGen {
     fun generate(event: GatherDataEvent) {
         val generator: DataGenerator = event.generator
         val packOutput = generator.packOutput
+        val existingFileHelper = event.existingFileHelper
 
         generator.addProvider(event.includeClient(), ModLanguageProvider(packOutput, "en_us"))
-        generator.addProvider(event.includeClient(), ModItemModelProvider(packOutput, event.existingFileHelper))
+        generator.addProvider(event.includeClient(), ModItemModelProvider(packOutput, existingFileHelper))
+        generator.addProvider(event.includeClient(), ModBlockStateProvider(packOutput, existingFileHelper))
     }
 }
 
@@ -30,5 +33,11 @@ class ModLanguageProvider(output: PackOutput, locale: String): LanguageProvider(
 class ModItemModelProvider(output: PackOutput, existingFileHelper: ExistingFileHelper): ItemModelProvider(output, Mindustry.MOD_ID, existingFileHelper) {
     override fun registerModels() {
         this.basicItem(ModItems.EXAMPLE_ITEM.get())
+    }
+}
+
+class ModBlockStateProvider(output: PackOutput, existingFileHelper: ExistingFileHelper): BlockStateProvider(output, Mindustry.MOD_ID, existingFileHelper) {
+    override fun registerStatesAndModels() {
+        this.simpleBlockWithItem(ModBlocks.POWER_NODE_BLOCK.get(), cubeAll(ModBlocks.POWER_NODE_BLOCK.get()))
     }
 }
